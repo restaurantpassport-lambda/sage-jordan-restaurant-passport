09d45d1226de0613ca33a7507730c68b11f01c29
sage-jordan <sagemjrodan@gmail.com>
2019-08-22 22:19:05 -0400

    Stuck on an error

--- restaurant-passport/src/index.js
+++ restaurant-passport/src/index.js
@@ -6,7 +6,11 @@ import App from './App';
 import Navigation from './components/Navigation'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render((<BrowserRouter><Navigation /></BrowserRouter>), document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <Navigation />
+    </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
