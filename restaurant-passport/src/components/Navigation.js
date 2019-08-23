95d8d036930233c39c1719732178b37d3ec2b210
sage-jordan <sagemjrodan@gmail.com>
2019-08-22 22:13:18 -0400

    added react-router-dom

--- restaurant-passport/src/components/Navigation.js
+++ restaurant-passport/src/components/Navigation.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Link, Component } from 'react';
 import { Menu, Segment } from 'semantic-ui-react';
 import Stamps from './Stamps';
 
@@ -18,25 +18,25 @@ export default class Navigation extends Component {
                 name='Passport'
                 active={activeItem === 'passport'}
                 onClick={this.handleItemClick}
-            />
+            ><Link to="/passport/"/></Menu.Item>
             <Menu.Item
                 name='Account'
                 active={activeItem === 'account'}
                 onClick={this.handleItemClick}
-            />
+            ><Link to="/account/"/></Menu.Item>
             <Menu.Menu position='right'>
                 <Menu.Item
                 name='LogIn'
                 active={activeItem === 'login'}
                 onClick={this.handleItemClick}
-                />
+                ><Link to="/login/"/></Menu.Item>
             </Menu.Menu>
             <Menu.Menu position='right'>
                 <Menu.Item
                 name='Sign Up'
                 active={activeItem === 'signup'}
                 onClick={this.handleItemClick}
-                />
+                ><Link to="/signup/"/></Menu.Item>
             </Menu.Menu>
             </Menu>
 
--- restaurant-passport/src/index.js
+++ restaurant-passport/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Navigation from './components/Navigation'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Navigation />, document.getElementById('root'));
+ReactDOM.render((<BrowserRouter><Navigation /></BrowserRouter>), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
