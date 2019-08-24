c19852d7af2f3468251d1f9727a72db96c6023d4
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-08-24 11:41:02 -0700

    Restaurant input refactored.

--- restaurant-passport/src/components/RestaurantInput.js
+++ restaurant-passport/src/components/RestaurantInput.js
@@ -6,38 +6,82 @@ import axios from "axios";
 
 export default function RestaurantInput() {
     const [newRes, setNewRes] = useState([]);
+    
+    const handleChange = event => {
+      setUser({ ...newRes, [event.target.name]: event.target.value });
+     
+  };
 
-    useEffect(() => {
-      if (status) {
-        setNewRes([...newRes, status]);
-      }
-    }, [status]);
-
+  const handleSubmit = event => {
+      event.preventDefault();
+      
+      axios
+      .post('https://build-restaurant-passport.herokuapp.com/users/register', newRes)
+      .then(res =>{
+          console.log(res)
+          localStorage.id = res.data.id
+      })
+      
+      setNewRes({
+        name: "", city: "", address: "", description: ""
+      })
+  };
     return(
         <div>
             <Form>
+            <Form.Group>
+                {/* name entry */}
+                <Form.Field
+                  control={Input}
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  value={newRes.name}
+                  onChange={handleChange}
+                  required
+                />     
 
-            {/* name entry */}
-          <Field component="input" type="text" name="name" placeholder="Restaurant Name"></Field>
-          
+                {/* city entry */}
+      
+                <Form.Field
+                    control={Input}
+                    type="text"
+                    name="city"
+                    placeholder="city"
+                    value={newRes.city}
+                    onChange={handleChange}
+                    required
+                  />   
 
-            {/* city entry */}
-          <Field component="input" type="text" name="city" placeholder="City"></Field>
-          
+                {/* address entry */}
+              
+                <Form.Field
+                  control={Input}
+                  type="text"
+                  name="address"
+                  placeholder="address"
+                  value={newRes.address}
+                  onChange={handleChange}
+                  required
+                />   
 
-            {/* address entry */}
-            <Field component="input" type="text" name="address" placeholder="Address"></Field>
-            
-
-            {/* description entry */}
-            <Field component="description" type="text" name="description" placeholder="Description"></Field>
-            
+                {/* description entry */}
+              
+                <Form.Field
+                  control={Input}
+                  type="text"
+                  name="description"
+                  placeholder="description"
+                  value={newRes.description}
+                  onChange={handleChange}
+                  required
+                />   
 
                 {/* city id entry? */}
 
-
-          <Button type="submit">Add</Button>
-            </Form>
+              <Form.Field control={Button}>Add</Form.Field>
+            </Form.Group>
+          </Form>
         </div>
     );
 };
