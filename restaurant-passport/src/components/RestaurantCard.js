bb48c6430042ad01b81df35ec5a8b6c5d0e7ecaa
sage-jordan <sagemjrodan@gmail.com>
2019-08-28 22:58:29 -0400

    Finished stamp functionality

--- restaurant-passport/src/components/RestaurantCard.js
+++ restaurant-passport/src/components/RestaurantCard.js
@@ -2,14 +2,11 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 
-
 const RestaurantCard = ({name}) => {
 
     return(
         <div className="restCardCont">
-            
             <p>Name: {name.charAt(0).toUpperCase()+ name.slice(1)}</p>
-            
             <p>City: Provo</p>
             <Button style={{color: 'black', backgroundColor: 'pink'} }className="btnHover">Stamp your passport</Button>
         </div>
